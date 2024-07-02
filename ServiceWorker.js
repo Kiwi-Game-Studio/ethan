f86357d5dcc9ac0752840d6aa4120bbb960abc70
Faizan <79058353+FaizanIdrees11@users.noreply.github.com>
2024-07-02 23:23:53 +0500

    Update ServiceWorker.js

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -5,6 +5,46 @@ const contentToCache = [
     "Build/pianoland.data",
     "Build/pianoland.wasm",
     "TemplateData/style.css"
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+
+      // Only cache supported schemes (http and https)
+      if (e.request.url.startsWith('http://') || e.request.url.startsWith('https://')) {
+          const cache = await caches.open(cacheName);
+          console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+          cache.put(e.request, response.clone());
+      }
+      
+      return response;
+    })());
+});
+
+
+
+/*const cacheName = "DefaultCompany-PianoLand-0.1";
+const contentToCache = [
+    "Build/pianoland.loader.js",
+    "Build/pianoland.framework.js",
+    "Build/pianoland.data",
+    "Build/pianoland.wasm",
+    "TemplateData/style.css"
 
 ];
 
@@ -30,4 +70,4 @@ self.addEventListener('fetch', function (e) {
       cache.put(e.request, response.clone());
       return response;
     })());
-});
+});*/
