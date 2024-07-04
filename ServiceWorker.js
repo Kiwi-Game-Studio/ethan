240c03f7c7a32005851e353be6ac87a8bc72c768
Faizan <79058353+FaizanIdrees11@users.noreply.github.com>
2024-07-04 20:22:37 +0500

    c

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,71 +1,44 @@
-const cacheName = "DefaultCompany-PianoLand-0.0.6";
-const contentToCache = [
-  "Build/pianoland_0.0.6.loader.js",
-  "Build/pianoland_0.0.6.framework.js",
-  "Build/pianoland_0.0.6.data",
-  "Build/pianoland_0.0.6.wasm",
-  "TemplateData/style.css",
-];
+const cacheName = "DefaultCompany-PianoLand-0.0.8";
 
-self.addEventListener("install", function (e) {
-  console.log("[Service Worker] Install");
-
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      console.log("[Service Worker] Caching app shell");
-      return cache.addAll(contentToCache);
-    })
-  );
-});
-
-self.addEventListener("activate", function (e) {
-  e.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (thisCacheName) {
-          if (thisCacheName !== cacheName) {
-            console.log(
-              "[Service Worker] Removing cached files from old cache:",
-              thisCacheName
-            );
-            return caches.delete(thisCacheName);
-          }
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all: app shell and content');
+            return cache.addAll([
+                "Build/pianoland_0.0.8.loader.js?version=1",
+                "Build/pianoland_0.0.8.framework.js?version=1",
+                "Build/pianoland_0.0.8.data?version=1",
+                "Build/pianoland_0.0.8.wasm?version=1",
+                "TemplateData/style.css?version=1"
+            ]);
         })
-      );
-    })
-  );
+    );
 });
 
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.match(e.request).then(function (response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('activate', function (e) {
+    console.log('[Service Worker] Activate');
+
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((name) => {
+                    if (name !== cacheName) {
+                        console.log('[Service Worker] Deleting old cache:', name);
+                        return caches.delete(name);
+                    }
+                })
+            );
+        })
+    );
 });
 
-self.addEventListener("fetch", function (e) {
-  e.respondWith(
-    caches.open(cacheName).then(function (cache) {
-      return cache.match(e.request).then(function (response) {
-        return (
-          response ||
-          fetch(e.request).then(function (response) {
-            // Check if the response is valid before caching
-            if (
-              !response ||
-              response.status !== 200 ||
-              response.type !== "basic"
-            ) {
-              return response;
-            }
-
-            cache.put(e.request, response.clone());
-            return response;
-          })
-        );
-      });
-    })
-  );
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+            return response || fetch(e.request);
+        })
+    );
 });
-
